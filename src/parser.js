2674f77ec59a32cdfb60c006029e0c3983d99cfa
Curtis Gagliardi <curtis@curtis.io>
2021-02-21 22:43:44 -0500

    use rustywind exclusively

--- src/parser.js
+++ src/parser.js
@@ -1,27 +1,17 @@
-import { sortClassInText } from './utils/sort'
-import fs from 'fs'
+import {spawnSync} from 'child_process'
 
-export function getParse(language, defaultParse) {
-	return (text, parsers, options) => {
-		let sortedText = text
-		if (fs.existsSync(options.classRegex) && fs.existsSync(options.classSorter)) {
-			const classRegex = JSON.parse(fs.readFileSync(options.classRegex, 'utf8'))
-			const classSorter = JSON.parse(fs.readFileSync(options.classSorter, 'utf8'))
+export function getParse(_language, defaultParse) {
+  return (text, parsers, options) => {
+    let p = spawnSync('rustywind', [ '--stdin' ], {input : text})
 
-			if (language in classRegex && Array.isArray(classSorter)) {
-				const classWrapperRegex = new RegExp(classRegex[language], 'gi')
-				sortedText = sortClassInText(text, classWrapperRegex, classSorter, {
-					removeDuplicates: options.removeDuplicatesClasses
-				})
-			}
-		}
+    let err_text = p.stderr.toString().trim()
+    if (err_text !== "") {
+      throw new Error(err_text)
+    }
 
-		const result = defaultParse(sortedText, parsers, options)
-
-		return result
-	}
+    let sorted_text = p.stdout.toString()
+    return defaultParse(sorted_text, parsers, options)
+  }
 }
 
-export default {
-	getParse
-}
+export default {getParse}
